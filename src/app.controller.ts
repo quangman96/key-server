a1b73017032a966a1c698264a20340ef5ce8fec9
mannq <mannq@aureole-it.vn>
2022-11-15 14:18:51 +0700

    update auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -7,6 +7,17 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return this.appService.getHello();
+    return this.appService.getText();
+  }
+
+  @Post('/auth')
+  checkAuth(@Body() body: { key: string }) {
+    console.log(body);
+    return this.appService.checkAuth(body['key']);
+  }
+
+  @Post('/keys')
+  getKeys(@Body() body: { private: string }) {
+    return this.appService.getKeys(body['key']);
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
-
+import { SECRET_KEY, KEYS } from './constant';
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getText(): string {
+    return 'KEY SERVER CREATED BY MANNQ !!!';
+  }
+
+  checkAuth(key: string): boolean {
+    if (!key) return false;
+    return KEYS.includes(key);
+  }
+
+  getKeys(key: string): string[] | boolean {
+    if (!key) return false;
+    if (SECRET_KEY !== key) return false;
+    return KEYS;
   }
 }
