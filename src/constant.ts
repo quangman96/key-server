f69e8c40446ababa3854b5ab12d00882b6000b8b
mannq <mannq@aureole-it.vn>
2022-11-18 14:07:27 +0700

    update key

--- src/constant.ts
+++ src/constant.ts
@@ -1,8 +1,8 @@
 /* eslint-disable prettier/prettier */
 export const KEYS = [
-  '981090e6-02bc-4e48-87cc-a14755914e29',
-  '687d21d0-4686-4532-b413-d9d3cf17fb22',
-  'a0cd1f5e-e841-4367-af19-e9f58183c0b6',
+  'mannq',
+  'bi',
+  'ben',
   '3c145c65-b193-4b3c-891f-2b7cb10e469c',
   'd3ba2921-0a00-418b-95df-04a25a383929',
 ];
